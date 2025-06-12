7b42256b715b7d473b433548271b621e1ed9e874
VaibhavDangaich <31512333+VaibhavDangaich@users.noreply.github.com>
2025-06-12 18:50:20 +0530

    added background in home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,19 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-white">
-      <h1 className="text-5xl font-bold mb-6 text-gray-800">EcoSync: Reduce Your Emissions</h1>
-      <h3 className=' text-gray-400 mb-10'>EcoSync is a smart platform designed to help Walmart suppliers reduce scope 3 emissions through data-driven insights and AI recommendations </h3>
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-white" style={{ backgroundImage: "url('/abstract.png')" }}>
+      <div className='flex items-baseline'>
+        <h1 className="text-5xl font-bold mb-6 text-gray-800">EcoSync: Reduce Your Emissions</h1>
+        <Image src="/leaf.png" width={50} height={50}></Image>
 
+      </div> 
+      
+      <h3 className=' text-gray-700 mb-10'>EcoSync is a smart platform designed to help Walmart suppliers reduce scope 3 emissions through data-driven insights and AI recommendations </h3>
+      
       <div className="flex gap-6">
         <Link
           href="/supplier"
-          className="bg-green-200 text-green-900 px-4 py-4 rounded-lg hover:bg-green-300 flex flex-col items-center"
+          className="bg-green-200 text-green-900 px-4 py-4 rounded-lg hover:bg-green-300 hover:scale-110 transition-all ease-in-out flex flex-col items-center"
         >
           <Image
             src="/supplier.jpg"
